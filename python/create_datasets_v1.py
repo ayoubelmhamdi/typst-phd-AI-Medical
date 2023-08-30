9c904cfc76ae77707c896edc7c535b956e23c9f1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-08-30 06:19:50 +0100

    (create_datasets_v1.py) update

--- python/create_datasets_v1.py
+++ python/create_datasets_v1.py
@@ -7,6 +7,8 @@ import pandas as pd
 import pylidc as pl
 from tqdm.auto import tqdm
 
+# import pydicom as dicom
+
 
 class ScanData:
     def __init__(self, paths=None):
@@ -14,7 +16,6 @@ class ScanData:
             raise KeyError("please provied paths list")
         self.scans = self.create_pylidcrc(paths)
         self.extract_data()
- 
 
     def create_pylidcrc(self, paths):
         scans = []
@@ -32,8 +33,8 @@ class ScanData:
 
     def extract_data(self):
         self.data = []
-        for i, scan in tqdm(enumerate(self.scans), total = scans.count()):
-            if i == 10:
+        for i, scan in tqdm(enumerate(self.scans), total=self.scans.count()):
+            if i == 4:
                 break
             nodules = scan.cluster_annotations()
             # for each scan.id we have many nodules, each nodules has many anns
@@ -66,26 +67,45 @@ class ScanData:
         df.to_csv(filename, index=False)
         return self
 
-    def save_roi_to_png(self, dir=None):
+    def save_roi_to_tiff(self, dir=None):
         if dir is None or dir == "":
             raise KeyError("you miss name of dir to store images")
         # TODO check if dir available
 
-        padding = [(30, 10), (10, 25), (0, 0)]
-        for i, row in tqdm(enumerate(self.data)):
+        # padding = [(30, 10), (10, 25), (0, 0)]
+        # padding = [(10,10,10)]
+        padding = [(0, 0), (0, 0), (0, 0)]
+        for i, row in tqdm(enumerate(self.data), total=len(self.data)):
+            vol, roi, bbox, ann = None, None, None, None
+            j=0
+            if j == 4:
+                break
             ann = row["ann"]
             bbox = ann.bbox(pad=padding)
+            vol = None
             vol = ann.scan.to_volume()
-            roi = vol[bbox][:, :, 2]
+            # print("vol.shape", vol.shape)
+            # print("vol[bbox].shape", vol[bbox].shape)
 
-            # Rescale the ROI image to the range of 0 to 255 for 8-bit images
-            roi = cv2.normalize(roi, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+            for region in range(vol[bbox].shape[2]):
+                roi = vol[bbox][:, :, region]
+                # Rescale the ROI image to the range of 0 to 255 for 8-bit images
+                roi = cv2.normalize(roi, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-            # Save the image as a PNG file in the patient directory
-            filename = row["roi_name"]
-            cv2.imwrite(filename, roi)
-            del vol, roi, bbox, ann
+                # Save the image as a PNG file in the patient directory
+                filename = row["roi_name"]
+                cv2.imwrite(f"{dir}/{filename}", roi)
             if i % 10:
                 gc.collect()
         return self
+
+
 # fin class
+paths = [
+    "/kaggle/input/lidcidri30/LIDC-IDRI-0001-0200",
+    #     "/kaggle/input/lidcidri30/LIDC-IDRI-0201-0400",
+    #     "/kaggle/input/lidcidri30/LIDC-IDRI-0401-0600",
+]
+scan_data = ScanData(paths)
+scan_data.write_to_csv("output01.csv")
+scan_data.save_roi_to_tiff("ROI3")
